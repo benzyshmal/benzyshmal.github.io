81d66c85071665a569383a889df3ed3bac96ea06
benzyshmal <benzlbenny2k@gmail.com>
2022-05-02 19:11:28 +0300

    vlassanov script V1.2

---
+++ js/vlassanov-script-v1.2.js
@@ -0,0 +1,9 @@
+
+
+var  greetingString = "Здарова";
+var myName = prompt("Пожалуйста, введите ваше блядское имя", '');
+var concatString;
+
+    document.write(greetingString + " " + myName + "<br />");
+    concatString = greetingString + " " + myName;
+    document.write(concatString);
